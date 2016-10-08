99d826f5b5822425bdda6c52d8cf30e31d0ed533
Tiago Teixeira <tiagocmtex@gmail.com>
2016-10-08 04:06:07 +0100

    Add css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+%w( home sessions ).each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+end
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
